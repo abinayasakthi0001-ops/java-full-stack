279020df4e8a8d6f5429821023e4f17cb548580b
Abinaya S <abinayasakthi0001@gmail.com>
2026-06-10 20:10:56 +0530

    Update for loops.java

--- for loops.java
+++ for loops.java
@@ -117,7 +117,62 @@ public class Abi {
 	 //   System.out.println();
   // }
    
-   
+
+	   // HOLLOW FULL PYRAMID
+    	
+//    	for(int i = 1;i<=5;i++) {                       <--      *
+//    	 	for(int j= 5;j>=i;j--) {                           *   * 
+//                System.out.print(" "+" ");                 *       *     
+//    	                                                  *            *
+//    		for (int j=1;j<=i-1;j++) {                   * * * * * * * * *
+//    			if((j==1)||(i==5)){
+//    				System.out.print("*"+" ");
+//    			}else {
+//    				System.out.print(" "+" ");
+//    			}
+//    	}
+//    		for(int j = 1;j<=i;j++) {
+//    		if((i==5)||(j==i)) {
+//    			System.out.print("*"+" ");
+//    		}else {
+//    			System.out.print(" "+" ");
+//    		}
+//    			
+//    		}System.out.println();
+//    		
+//    	}
+    	
+    	
+//    	half pyramid in num                       <---- 1
+//    	                                                1 2
+//     for(int i = 1;i<=5;i++) {                        1 2 3
+//    	 for(int j = 1;j<=i;j++) {                      1 2 3 4
+//    		 System.out.print(j+" ");                   1 2 3 4 5
+//    	 }System.out.println();
+//     }
+     
+    
+//    	inverted half pyramid in num                <--- 5 4 3 2 1
+//                                                       5 4 3 2 
+//     for(int i =1;i<=5;i++) {                          5 4 3
+//    	 for(int j = 5;j>=i;j--) {                       5 4
+//    		 System.out.print(i+" ");                    5
+//    	 }System.out.println();
+//     }
+     
+     
+//    	hollow half pyramid in num                   <---     *
+//                                                            * *
+//    	for(int i = 1;i<=5;i++) {                             *   * 
+//       	 for(int j = 1;j<=i;j++) {                        *     *
+//       		                                              * * * * *
+//       		if((j==1)||(j==i)||(i==5)) {
+//                System.out.print("*"+" ");
+//       		 }else {
+//       			System.out.print(" "+" ");
+//       		 }
+//       	 }System.out.println();
+//        }
    
    
    
