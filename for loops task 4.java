a62b359765cb41d6a41282e0d72e32b81096dfde
Abinaya S <abinayasakthi0001@gmail.com>
2026-06-12 18:00:10 +0530

    Update for loops task 4.java

--- for loops task 4.java
+++ for loops task 4.java
@@ -188,22 +188,10 @@ public class Abi {
 //    	 for(int j = 5;j>=i;j--) {                       5 4
 //    		 System.out.print(i+" ");                    5
 //    	 }System.out.println();
-//     }
-     
-
-	   
-//    	hollow half pyramid in num                   <---     *
-//                                                            * *
-//    	for(int i = 1;i<=5;i++) {                             *   * 
-//       	 for(int j = 1;j<=i;j++) {                        *     *
-//       		                                              * * * * *
-//       		if((j==1)||(j==i)||(i==5)) {
-//                System.out.print("*"+" ");
-//       		 }else {
-//       			System.out.print(" "+" ");
-//       		 }
-//       	 }System.out.println();
-//        }
+    // }
+     //     ^
+            |
+	
    
    
    
