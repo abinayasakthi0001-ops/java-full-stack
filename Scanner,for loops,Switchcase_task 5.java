cb1c1b8121a06b569e8ed43858fbb8f01aea962a
Abinaya S <abinayasakthi0001@gmail.com>
2026-06-21 01:56:27 +0530

    Add files via upload

---
+++ Scanner,for loops,Switchcase_task 5.java
@@ -0,0 +1,404 @@
+package Abinn;
+import java.util.Scanner;
+
+public class Scanner_task {
+
+	 public static void main(String args[]) {
+		 
+//		 Scanner input
+		 
+//		 student details name,age,dept
+		 
+		 Scanner scan = new Scanner(System.in);
+//		 
+//		 System.out.println("enter the input");
+//		 String name = scan.nextLine();
+//		 byte age = scan.nextByte();
+//		 scan.nextLine();
+//		 String dept = scan.nextLine();
+//		 
+//		  System.out.println("Student name : "+name);
+//		  System.out.println("Student age : "+age);
+//		  System.out.println("Student's department : "+dept);
+//		  
+		 
+//		 Mobile purchase -mobile name ,price 
+		 
+//		 Scanner scan= new Scanner(System.in);
+//		 
+//		 System.out.println("welcome to our mobile store !");
+//		 System.out.println("which mobile brand are you looking for?");
+//		 String brand = scan.nextLine();
+//		 int price = scan.nextInt();
+//		 
+//		 System.out.println("Mobile brand : "+brand);
+//		 System.out.println("Mobile price : "+price);
+//		 
+		 
+		 
+//		 Employee name,Id ,salary
+		 
+//		 String name=scan.nextLine();
+//		 int ID = scan.nextInt();
+//		 scan.nextLine();
+//		 int salary= scan.nextInt();
+//		 
+//		 System.out.println("Employee's name : "+name);
+//		 System.out.println("Employee's ID : "+ID);
+//		 System.out.println("Employee's salary : "+salary);
+		 
+		 
+		 
+//		 college admission student name,10th mark,12th mark 
+		 
+//		 System.out.println("enter the input");
+//		 String name = scan.nextLine();
+//		 int mark_10= scan.nextInt();
+//		 int mark_12 = scan.nextInt();
+//		 
+//		 System.out.println("Student's name : "+name);
+//		 System.out.println("Student's 10th mark : "+mark_10+"/500");
+//		 System.out.println("Student's 12th mark : "+mark_12+"/600");
+//          		 
+		 
+		 
+		 
+//		 Product bill Quantity,name ,price
+		 
+//		 System.out.println("enter 1st product");
+//		 
+//		 String product1 = scan.nextLine();
+//		 int quantity1 = scan.nextInt();
+//		 int price1 = scan.nextInt();
+//		 
+//		 System.out.println(" enter 2nd product");
+//		 scan.nextLine();
+//		 String product2 = scan.nextLine();
+//		
+//		 int quantity2 = scan.nextInt();
+//		 int price2 = scan.nextInt();
+//		 
+//		 System.out.println("1st product name : "+product1);
+//		 System.out.println("1st product quantity : "+quantity1);
+//		 System.out.println("1st product price : "+price1);
+//		 System.out.println();
+//		System.out.println("2nd product name : "+product2);
+//		System.out.println("2nd product quantity : "+quantity2);
+//		System.out.println("2nd product price : "+price2);
+//		System.out.println();
+//		System.out.println("total price : "+(price1+price2));
+		
+		 
+		 
+//		 SWITCH CASE 
+		 
+//		 Day finder 
+//		 
+//		 System.out.println("enter your day number");
+//		 int day = scan.nextInt();
+//		 
+//		 switch(day) {
+//		  
+//		 case 1:System.out.println("your day "+day+" is Sunday");break;
+//		 case 2:System.out.println("your day "+day+" is Monday");break;
+//		 case 3:System.out.println("your day "+day+" is Tuesday");break;
+//		 case 4:System.out.println("your day "+day+" is Wednesday");break;
+//		 case 5:System.out.println("your day "+day+" is Thursday");break;
+//		 case 6:System.out.println("your day "+day+" is Friday");break;
+//		 case 7:System.out.println("your day "+day+" is Saturday");break;
+//		 default:System.out.println("give valid day number.....");
+//		 }
+		 
+		 
+//		 ATM menu 
+		 
+//		 System.out.println("enter your choice ");
+//		 byte choice =scan.nextByte();
+//		 
+//		 switch(choice){
+//		 case 1:System.out.println("you selected balance enquiry");break;
+//		 case 2:System.out.println("you selected cash withdrawal");break;
+//		 case 3:System.out.println("you selected cash deposit");break;
+//		 case 4:System.out.println("you selected mini statement");break;
+//		 case 5:System.out.println("thank you for using ATM");break;
+//		 default:System.out.println("invalid choice! please enter the valid option");
+//		 }
+		 
+		 
+//		 course selection 
+//		
+//		System.out.println("welcome to the college");
+//		System.out.println("may I know your interested course");
+//		
+//		byte course = scan.nextByte();
+//		
+//		switch(course) {
+//		case 1:System.out.println("you selected Bsc computer science course");break;
+//		case 2:System.out.println("you selected Cse computer science course");break;
+//		case 3:System.out.println("you selected Bsc IT course ");break;
+//		case 4:System.out.println("you selected Msc computer science");break;
+//		case 5:System.out.println("you selected Msc Biotechnology course");break;
+//		default:System.out.println("please select the available course");
+//		}
+		 
+		 
+		 
+//		 food menu 
+//		 
+//		 System.out.println("please select your food in the food menu");
+//		 byte food = scan.nextByte();
+//		 
+//		 switch(food) {
+//		 case 1:System.out.println("you selected the mexican Pizza");break;
+//		 case 2:System.out.println("you selected the fried chicken Burger");break;
+//		 case 3:System.out.println("you selected the chicken maggie");break;
+//		 case 4:System.out.println("you selected the sizzling Brownie");break;
+//		 case 5:System.out.println("you selected the panneer shawarma");break;
+//		 case 6:System.out.println("you selected the chicken wrap");break;
+//		 default:System.out.println("Please select the available food");
+//		 }
+		 
+		 
+		 
+//		 Employee department 
+//		 
+//		 System.out.println("enter your department");
+//		 
+//		 byte dept = scan.nextByte();
+//		 switch(dept) {
+//		 case 1:System.out.println("your department is Human resourse");break;
+//		 case 2:System.out.println("your department is Finance");break;
+//		 case 3:System.out.println("your department is Information Technology");break;
+//		 case 4:System.out.println("your department is Marketing");break;
+//		 case 5:System.out.println("your department is Sales");break;
+//		 default:System.out.println("please select the valid Department");
+//		 }
+		 
+		 
+//		 calculator
+//		 
+//		 System.out.println("Enter the values");
+//		 byte a = scan.nextByte();
+//		 byte b = scan.nextByte();
+//		 System.out.println("enter the operator");
+//		 byte op = scan.nextByte();
+//		 
+//		 switch (op) {
+//		 case 1:System.out.println("Addition : "+(a+b));break;
+//		 case 2:System.out.println("Substraction : "+(a-b));break;
+//		 case 3:System.out.println("Multiplication : "+(a*b));break;
+//		 case 4:System.out.println("Division : "+(a/b));break;
+//		 default:System.out.println("please enter the valid operator");
+//		 }
+		 
+		 
+		 
+//		 Bank services
+//		 
+//        System.out.println("enter your services");
+//        
+//        byte service = scan.nextByte();
+//        
+//        switch(service) {
+//        case 1:System.out.println("you selected cash deposit");break;
+//        case 2:System.out.println("you selected Balance enquiry");break;
+//        case 3:System.out.println("you selected cash withdrawal");break;
+//        case 4:System.out.println("you selected mini statement");break;
+//        case 5:System.out.println("you selected Account Opening");break;
+//        default:System.out.println("please select the valid service");break;
+//        }
+		 
+		 
+		 
+//		 For loop tasks
+		 
+//		 Print numbers from 1 to 20.
+//		 
+//		 for(int i=1;i<=20;i++) {
+//              System.out.println(i);
+//		 }
+		 
+		 
+//		 Print even numbers from 2 to 20.
+//		 
+//		 for(int i = 2;i<=20;i++) {
+//             if(i%2==0) {
+//                System.out.println(i);
+//             }
+//		 }
+		 
+		 
+//		 Print odd numbers from 1 to 25
+//		 
+//		 for(int i = 1;i<=25;i++) {
+//           if(i%2==1) {
+//              System.out.println(i);
+//           }
+//		 }
+		 
+		 
+		 
+//		 Print multiplication table for a given number
+//		 
+//		 byte a = 2;
+//		 for(int i = 1;i<=10;i++) {
+//			 System.out.println(i+"*"+a+"="+" "+(a*i));
+//		 }
+		 
+		 
+		 
+//		 Find sum of numbers from 1 to 100
+//		 
+//		 int sum = 0;
+//		 for(int i = 1;i<=100;i++) {
+//		sum=sum+i; 
+//		if(i==100) {
+//			System.out.println("Total sum : "+sum);
+//		 }
+//		 }
+		 
+		 
+//		 Print squares of numbers from 1 to 10
+//		 
+//		 for(int i = 1;i<=10;i++) {
+//			 System.out.println("Square of "+i+"="+" "+i*i);
+//		 }
+		 
+		 
+		 
+//		 Print numbers from 20 to 1
+//		 
+//		 for(int i=20;i>=1;i--) {
+//           System.out.println(i);
+//		 }
+		 
+		 
+//		 Print multiples of 5 from 0 to 100
+//		 
+//		 int a = 5;
+//		 for(int i = 0;i<=100;i++) {
+//			 System.out.println(i+"*"+a+"="+" "+(a*i));
+//		 }
+		 
+		 
+		 
+//		 MINI REAL TIME TASKS 
+		 
+//		 Mobile Store Menu - Display offers based on brand selection
+//		 
+//		 System.out.println("welcome to our mobile store");
+//		 System.out.println("select the mobile brand");
+//		 String brand = scan.nextLine();
+//		 
+//		 System.out.println("enter the offer");
+//		 byte offer = scan.nextByte();
+//		 System.out.println("your selected brand is "+brand);
+//		 System.out.println();
+//		 switch(offer) {
+//		 case 1:System.out.println("15% offer for Samsung Galaxy");break;
+//		 case 2:System.out.println("10% offer for Oppo");break;
+//		 case 3:System.out.println("17% offer for Redmi");break;
+//		 case 4:System.out.println("25% offer for iphone");break;
+//		 case 5:System.out.println("12% offer for Realme");break;
+//		 default:System.out.println("please choose an available Mobile brands");
+//		 }
+	
+		 
+		 
+//		 Student Mark Analysis - Accept 5 marks and calculate total
+//		 
+//		 System.out.println("Enter the marks");
+//		 int m1 = scan.nextInt();
+//		 int m2 = scan.nextInt();
+//		 int m3 = scan.nextInt();
+//		 int m4 = scan.nextInt();
+//		 int m5 = scan.nextInt();
+//		 
+//		 System.out.println("Total marks = "+(m1+m2+m3+m4+m5));
+		 
+		 
+//		 Attendance Counter - Print attendance numbers from 1 to 30
+//		 
+//		 System.out.println("Attendance counter");
+//		 for(int i=1;i<=30;i++) {
+//            System.out.println(i);
+//		 }
+		 
+		 
+//		 Shopping Bill - Calculate total bill using price and quantity
+//		 
+//		 System.out.println("Shopping Bill");
+//		 
+//		 System.out.println("enter 1st product");
+//		 
+//		 String product1 = scan.nextLine();
+//		 int quantity1 = scan.nextInt();
+//		 int price1 = scan.nextInt();
+//		 
+//		 System.out.println(" enter 2nd product");
+//		 scan.nextLine();
+//		 String product2 = scan.nextLine();
+//		
+//		 int quantity2 = scan.nextInt();
+//		 int price2 = scan.nextInt();
+//		 
+//		 System.out.println("1st product name : "+product1);
+//		 System.out.println("1st product quantity : "+quantity1);
+//		 System.out.println("1st product price : "+price1);
+//		 System.out.println();
+//		System.out.println("2nd product name : "+product2);
+//		System.out.println("2nd product quantity : "+quantity2);
+//		System.out.println("2nd product price : "+price2);
+//		System.out.println();
+//		System.out.println("total Bill : "+(price1+price2));
+		 
+		 
+		 
+//		 Restaurant Menu - Display selected food item and price.
+//		 
+//		 System.out.println("Restaurent Menu");
+//		 System.out.println("please select your food in the menu");
+//		 byte food = scan.nextByte();
+//		 int price = scan.nextInt();
+//		 
+//		 switch(food) {
+//		 case 1:System.out.println("you selected the mexican Pizza");break;
+//		 case 2:System.out.println("you selected the fried chicken Burger");break;
+//		 case 3:System.out.println("you selected the chicken maggie");break;
+//		 case 4:System.out.println("you selected the sizzling Brownie");break;
+//		 case 5:System.out.println("you selected the panneer shawarma");break;
+//		 case 6:System.out.println("you selected the chicken wrap");break;
+//		 default:System.out.println("Please select the available food");
+//		 }
+//		 System.out.println("price of the food : "+price);
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+	 }
+}
